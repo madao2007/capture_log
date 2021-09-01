af4f552873ed28276c05ade68e4d154e76d0f92d
Richuang <richuang@cisco.com>
2021-09-01 17:44:34 +0800

    add cap_log.py

---
+++ cap_log.py
@@ -0,0 +1,16 @@
+
+
+
+
+def device_login(host, user, password, enable_password):
+    pass
+
+
+def main():
+    enable_password = ""
+    device_login(host, user, password, enable_password)
+
+
+
+if __name__ == "__main()__":
+    main()
