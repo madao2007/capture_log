02dc622c93d39a86e684df6a9b7c4fc29cfb3e10
Richuang <richuang@cisco.com>
2021-09-02 20:39:24 +0800

    firt release

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # capture_log
+
+## How to use
+1. Install python module
+```shell
+pip install -r requirements.txt
+```
+2. Modify the `device_list.csv`
+3. Modify the `command_list.csv`
+4. Run the scripts
+```shell
+python3 cap_log.py
+```
+5. Find the logs in `show-output` folder
--- cap_log.py
+++ cap_log.py
@@ -1,34 +1,70 @@
 import csv
+import paramiko
+import time
+from datetime import datetime
 
-def read_file():
-    with open('device_list.csv') as csv_file:
+def read_file(file_name):
+    with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         list_of_rows = list(csv_reader)
         return list_of_rows
 
-def device_login(device_info):
+
+def capture_log(device_info):
     host_ip = device_info[0]
     username = device_info[1]
     password = device_info[2]
     device_type = device_info[3]
+    device_outputs = ''
+    enable_password = ''
+
+    print('Working on ' + host_ip)
+    print('...')
 
     if len(device_info) == 5 and device_type == 'ios':
         enable_password =  device_info[4]
-    else:
-        enable_password = "]"
+        
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+    ssh.connect(hostname=host_ip, port=22, username=username, password=password)
+    connection = ssh.invoke_shell()
+    if device_type == 'ios':
+        prompt_out = str(connection.recv(9999))
+        prompt_out = prompt_out.split('\n')[-1]
+        if '#' not in prompt_out:
+            connection.send("enable")
+            connection.send('\n')
+            time.sleep(1)
+            connection.send(enable_password) 
+            connection.send('\n')
 
+    command_list = read_file('command_list.csv')
+    for command in command_list:
+        connection.send(command[0])
+        connection.send('\n')
+        print(command[0])
+        time.sleep(5)
+        file_output = connection.recv(9999999).decode(encoding='utf-8')
+        device_outputs += file_output
+    curret_date = datetime.today().strftime('%Y-%m-%d')
+    file_name = curret_date + '_' + host_ip
 
-def device_cmd_execute():
-    pass
+    save_file(file_name, device_outputs)
 
-def save_output_to_file():
-    pass
+def save_file(file_name, content):
+    file = './show-output/' + file_name + '.txt'
+    f = open(str(file), 'a+')
+    f.write(content)
+    f.close()
+        
 
 def main():
-    device_info_list = read_file()
+    device_info_list = read_file('device_list.csv')
     for device_info in device_info_list:
-        device_login(device_info)
+        capture_log(device_info)
+        print('Done')
+        print('=' * 15)
 
 if __name__ == '__main__':
     main()
